e930c58cdd8271a0f5e83715e0aff1656de0de7c
Nathan Schneider <nlschneider@gmail.com>
2019-11-08 00:23:23 -0500

    changed ip retrieval

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const app = express()
 const port = 8888
 const webshot = require('webshot');
 var os = require("os");
-var hostname = os.hostname();
+var ifaces = os.networkInterfaces();
 
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+  
+    ifaces[ifname].forEach(function (iface) {
+      if ('IPv4' !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
+  
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ':' + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
+  });
 
 app.use(express.static('images'))
 
@@ -15,7 +34,7 @@ app.get('/url/:url', function (req, res) {
          if (err){
 
          }
-         res.send(`${hostname}/images/${filename}.png`)
+         res.send(`${iface.address}/images/${filename}.png`)
       });
   })
 
